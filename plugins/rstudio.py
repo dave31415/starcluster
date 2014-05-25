624b4bc7acfbb7c86f4015971ba34a537aed36bb
user <no.email>
2014-05-25 17:41:02 -0400

    got R 3.0 working on ubuntu, finished R, rstudio and shiny plugins

--- plugins/rstudio.py
+++ plugins/rstudio.py
@@ -7,13 +7,29 @@ def authorize_port(node,port):
     node.ec2.conn.authorize_security_group(group_id=group.id, ip_protocol='tcp', 
         from_port=port,to_port=port, cidr_ip=world_cidr)
 
+
+class RInstaller(ClusterSetup):
+    '''Install R'''
+    def __init__(self):
+        log.info('Installing R and some packages')
+        self.cran=['codetools','Rcpp','RJSONIO','bitops','caTools','digest','xtable']
+         
+        
+    def run(self, nodes, master, user, user_shell, volumes):
+        for node in nodes:
+            log.info("Installing %s on %s" % ("R", node.alias))
+            node.ssh.execute("wget https://raw.githubusercontent.com/dave31415/downloads/master/install_R_on_ubuntu.sh")
+            node.ssh.execute("wget https://raw.githubusercontent.com/dave31415/downloads/master/install_R_packages.R")
+            node.ssh.execute("chmod u+x install_R_on_ubuntu.sh")
+            node.ssh.execute("./install_R_on_ubuntu.sh")
+            
 class RStudioServerInstaller(ClusterSetup):
     '''Install R and RStudio Server'''
     def __init__(self):
         log.info('Installing Rstudio Server on port 8787 of each node')
         
         self.port=8787
-        self.debian_pkgs="r-base-core gdebi-core libapparmor1"
+        self.debian_pkgs="gdebi-core libapparmor1"
         self.pkg_file="rstudio-server-0.98.507-amd64.deb"
         self.root_url="http://download2.rstudio.org"
         self.url=self.root_url+'/'+self.pkg_file
@@ -29,7 +45,8 @@ class RStudioServerInstaller(ClusterSetup):
             node.ssh.execute("rstudio-server verify-installation")
             #the following assumes the rstudio user exists, usually done in config file
             node.ssh.execute("echo rstudio:beachworks | chpasswd")
-            authorize_port(node,self.port)
+        #only need to authorize the group once
+        authorize_port(node,self.port)
             
 class ShinyServerInstaller(ClusterSetup):
     '''Install Shiny and R if not installed'''
@@ -37,7 +54,7 @@ class ShinyServerInstaller(ClusterSetup):
         log.info('Installing Shiny Server on port 3838 of each node')
         
         self.port=3838
-        self.debian_pkgs="r-base-core gdebi-core"
+        self.debian_pkgs="gdebi-core"
         self.pkg_file="shiny-server-1.1.0.10000-amd64.deb"
         self.root_url="http://download3.rstudio.org/ubuntu-12.04/x86_64"
         self.url=self.root_url+'/'+self.pkg_file
@@ -45,13 +62,14 @@ class ShinyServerInstaller(ClusterSetup):
     def run(self, nodes, master, user, user_shell, volumes):
         for node in nodes:
             #R and Rstudio
-            log.info("Installing %s on %s" % ("Rstudio Server", node.alias))
+            log.info("Installing %s on %s" % ("Shiny Server", node.alias))
             node.ssh.execute('sudo apt-get -y install %s' % self.debian_pkgs)
-            node.ssh.execute(''' sudo su - -c "R -e \"install.packages('shiny', repos='http://cran.rstudio.com/')\"" ''')
+            #having trouble with this line, quotes
             node.ssh.execute("wget %s" % self.url)
             node.ssh.execute("sudo gdebi -n %s" % self.pkg_file)
             node.ssh.execute("rm %s" % self.pkg_file)
             #the following assumes the shiny user exists, usually done in config file
             node.ssh.execute("echo shiny:beachworks | chpasswd")
-            authorize_port(node,self.port)
+        #only need to authorize the group once
+        authorize_port(node,self.port)
                  
