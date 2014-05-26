35d27008b38fc75770c7a50f5545efca30a2d288
user <no.email>
2014-05-25 23:03:51 -0400

    wrote spark plugin

--- plugins/rstudio.py
+++ plugins/rstudio.py
@@ -1,25 +1,22 @@
 from starcluster.clustersetup import ClusterSetup
 from starcluster.logger import log
 
-def authorize_port(node,port):
+def authorize_port(node,port,cidr = '0.0.0.0/0'):
     group = node.cluster_groups[0]
-    world_cidr = '0.0.0.0/0'
     node.ec2.conn.authorize_security_group(group_id=group.id, ip_protocol='tcp', 
-        from_port=port,to_port=port, cidr_ip=world_cidr)
-
+        from_port=port,to_port=port, cidr_ip=cidr)
 
 class RInstaller(ClusterSetup):
     '''Install R'''
     def __init__(self):
         log.info('Installing R and some packages')
         self.cran=['codetools','Rcpp','RJSONIO','bitops','caTools','digest','xtable']
-         
         
     def run(self, nodes, master, user, user_shell, volumes):
         for node in nodes:
             log.info("Installing %s on %s" % ("R", node.alias))
-            node.ssh.execute("wget https://raw.githubusercontent.com/dave31415/downloads/master/install_R_on_ubuntu.sh")
-            node.ssh.execute("wget https://raw.githubusercontent.com/dave31415/downloads/master/install_R_packages.R")
+            node.ssh.execute("wget https://raw.githubusercontent.com/dave31415/downloads/master/install_R_on_ubuntu.sh &> wget.log")
+            node.ssh.execute("wget https://raw.githubusercontent.com/dave31415/downloads/master/install_R_packages.R &> wget.log")
             node.ssh.execute("chmod u+x install_R_on_ubuntu.sh")
             node.ssh.execute("./install_R_on_ubuntu.sh")
             
@@ -39,7 +36,7 @@ class RStudioServerInstaller(ClusterSetup):
             #R and Rstudio
             log.info("Installing %s on %s" % ("Rstudio Server", node.alias))
             node.ssh.execute('sudo apt-get -y install %s' % self.debian_pkgs)
-            node.ssh.execute("wget %s" % self.url)
+            node.ssh.execute("wget %s &> wget.log"% self.url)
             node.ssh.execute("sudo gdebi -n %s" % self.pkg_file)
             node.ssh.execute("rm %s" % self.pkg_file)
             node.ssh.execute("rstudio-server verify-installation")
@@ -51,8 +48,7 @@ class RStudioServerInstaller(ClusterSetup):
 class ShinyServerInstaller(ClusterSetup):
     '''Install Shiny and R if not installed'''
     def __init__(self):
-        log.info('Installing Shiny Server on port 3838 of each node')
-        
+        log.info('Installing Shiny Server on port 3838 of each node and a webserver')
         self.port=3838
         self.debian_pkgs="gdebi-core"
         self.pkg_file="shiny-server-1.1.0.10000-amd64.deb"
@@ -63,13 +59,22 @@ class ShinyServerInstaller(ClusterSetup):
         for node in nodes:
             #R and Rstudio
             log.info("Installing %s on %s" % ("Shiny Server", node.alias))
-            node.ssh.execute('sudo apt-get -y install %s' % self.debian_pkgs)
-            #having trouble with this line, quotes
-            node.ssh.execute("wget %s" % self.url)
-            node.ssh.execute("sudo gdebi -n %s" % self.pkg_file)
-            node.ssh.execute("rm %s" % self.pkg_file)
-            #the following assumes the shiny user exists, usually done in config file
-            node.ssh.execute("echo shiny:beachworks | chpasswd")
-        #only need to authorize the group once
-        authorize_port(node,self.port)
-                 
+            if False:
+                node.ssh.execute('sudo apt-get -y install %s' % self.debian_pkgs)
+                #having trouble with this line, quotes
+                node.ssh.execute("wget %s &> wget.log" % self.url)
+                node.ssh.execute("sudo gdebi -n %s" % self.pkg_file)
+                node.ssh.execute("rm %s" % self.pkg_file)
+                #the following assumes the shiny user exists, usually done in config file
+                node.ssh.execute("echo shiny:beachworks | chpasswd")
+            if True:
+                #shiny gives you a free webserver
+                node.ssh.execute("mkdir -p /srv/shiny-server/www")
+                node.ssh.execute("mkdir -p /srv/shiny-server/www/temp")
+                node.ssh.execute("mkdir -p /srv/shiny-server/www/info")
+                node.ssh.execute(
+                "echo \<html\>\<p\>Hello World from %s\</p\>\</html\> > /srv/shiny-server/www/temp/index.html" % node.alias)
+            
+        #only need to authorize the group once so do it at end
+        authorize_port(node,self.port) 
+        
---
+++ plugins/spark.py
@@ -0,0 +1,39 @@
+from starcluster.clustersetup import ClusterSetup
+from starcluster.logger import log
+
+class SparkInstaller(ClusterSetup):
+    '''Install Spark'''
+    def __init__(self):
+        log.info('Installing Spark')
+        self.spark_dir="spark-0.9.1-bin-hadoop1"
+        self.url="http://d3kbcqa49mib13.cloudfront.net/%s.tgz" % self.spark_dir
+
+    def run(self, nodes, master, user, user_shell, volumes):
+        open_up_all_ports()
+        for node in nodes:
+            log.info("Installing %s on %s" % ("Spark", node.alias))
+            node.ssh.execute("wget %s &> wget.log" % self.url)
+            node.ssh.execute("sleep 2")
+            node.ssh.execute("tar xfvz %s.tgz" % self.spark_dir)
+            node.ssh.execute("echo %s > %s/conf/slaves" % (node.alias,spark_dir))
+        master.ssh.execute("%s/sbin/start-all.sh" % self.spark_dir)
+        
+    def open_up_all_ports(protocols=['tcp','udp','icmp']):
+        #this might not be very secure, revisit
+        log.info("Opening up all ports for Spark!")
+    
+        group = node.cluster_groups[0]
+
+        if 'tcp' in protocols:
+            node.ec2.conn.authorize_security_group(group_id=group.id, ip_protocol='tcp', 
+            from_port=1,to_port=65535, cidr_ip='0.0.0.0/0')
+        
+        if 'udp' in protocols:
+            node.ec2.conn.authorize_security_group(group_id=group.id, ip_protocol='udp', 
+            from_port=1,to_port=65535, cidr_ip='0.0.0.0/0')
+        
+        if 'icmp' in protocols:
+            node.ec2.conn.authorize_security_group(group_id=group.id, ip_protocol='icmp', 
+            from_port=-1,to_port=-1, cidr_ip='0.0.0.0/0')
+        
+                
